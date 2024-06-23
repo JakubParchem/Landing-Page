cd26e244fe41befa739f7581336feaf362896bd3
JakubParchem <jakubparchem@gmail.com>
2024-06-23 15:45:38 +0200

    added "call to action"

--- en/index.php
+++ en/index.php
@@ -6,11 +6,14 @@
     <link rel="stylesheet" href="../style.css">
     <link rel="icon" href="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719068400&semt=sph">
 </head>
-<?php include "header.html"?>
+<?php include "header.html";?>
+<?php include "call.html";?>
+<div id="opinion">
 <?php include "../classes/Opinions.php";
 $opinions=new Opinions();
 $opinions->show(3);
 ?>
-<?php include "opinion.html"?>
-<?php include "contact.html"?>
+<?php include "opinion.html";?>
+</div>
+<?php include "contact.html";?>
 </html>
