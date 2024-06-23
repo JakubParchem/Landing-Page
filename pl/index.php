11a2c0b2114009712a68070c156e294173474338
JakubParchem <jakubparchem@gmail.com>
2024-06-23 16:29:34 +0200

    changed title

--- pl/index.php
+++ pl/index.php
@@ -2,7 +2,7 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Jakaś Firma</title>
+    <title>SomeCompany inc.</title>
     <link rel="stylesheet" href="../style.css">
     <link rel="icon" href="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719068400&semt=sph">
 </head>
